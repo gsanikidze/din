6107ce27b7a849efc387cfb3f828bfca0192ce22
gsanikidze <sanikidze.guram@gmail.com>
2022-06-23 14:34:57 +0400

    [new] add todo design

--- Shared/Screens/AddTodoScreen.swift
+++ Shared/Screens/AddTodoScreen.swift
@@ -8,8 +8,62 @@
 import SwiftUI
 
 struct AddTodoScreen: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    private let groups = [
+        "Today", "Some group", "Some other group"
+    ]
+    
+    @State private var title = ""
+    @State private var group = ""
+    @State private var date = Date()
+    
     var body: some View {
-        Text("Add todo screen")
+        NavigationView {
+            ScrollView {
+                VStack (spacing: 20) {
+                    VStack (alignment: .center, spacing: 20) {
+                        TextField("Title", text: $title)
+                        Divider()
+                        Picker("Select Group", selection: $group) {
+                            ForEach(groups, id: \.self) {
+                                Text($0).foregroundColor(.red)
+                            }
+                        }
+                    }
+                    .padding()
+                    .background(Color(UIColor.systemGray5))
+                    .cornerRadius(10)
+                    
+                    VStack (alignment: .center, spacing: 20) {
+                        DatePicker(
+                                "Do Date",
+                                selection: $date,
+                                displayedComponents: [.date]
+                            )
+                    }
+                    .padding()
+                    .background(Color(UIColor.systemGray5))
+                    .cornerRadius(10)
+                }
+                .padding()
+            }
+            .toolbar {
+                ToolbarItem (placement: .navigationBarLeading) {
+                    Button (action: { dismiss() }) {
+                        Text("Cancel")
+                    }
+                }
+                
+                ToolbarItem (placement: .primaryAction) {
+                    Button (action: { dismiss() }) {
+                        Text("Done")
+                    }
+                }
+            }
+            .navigationTitle("Add Todo")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
