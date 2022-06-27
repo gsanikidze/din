71ad7ba4181a3bb8ed8bbbb6199944c92de2ce96
gsanikidze <sanikidze.guram@gmail.com>
2022-06-27 17:12:41 +0400

    [new] bar chart

--- Shared/Screens/ChartTabScreen.swift
+++ Shared/Screens/ChartTabScreen.swift
@@ -32,39 +32,74 @@ struct ChartTabScreen: View {
         }
     }
     
-    private let style = ChartStyle(backgroundColor: Color(UIColor.systemGray6), accentColor: .accentColor, secondGradientColor: .secondary, textColor: .primary, legendTextColor: .primary, dropShadowColor: .black)
+    private var todosByGroup: [(String, Double)] {
+        get {
+            var res: [(String, Double)] = []
+            
+            for group in groups {
+                let todosCount = Double(group.todos?.count ?? 0)
+                
+                res.append(("\(group.title!) (\(Int(todosCount)))", todosCount))
+            }
+            
+            return res
+        }
+    }
+    
+    private var totalTodos: Int {
+        get {
+            var res = 0
+            
+            for group in groups {
+                res += group.todos!.count
+            }
+            
+            return res
+        }
+    }
+    
+    private let style = ChartStyle(backgroundColor: Color(UIColor.systemGray6), accentColor: .indigo.opacity(0.5), secondGradientColor: .indigo, textColor: .primary, legendTextColor: .primary, dropShadowColor: .black.opacity(0))
     
     var body: some View {
         NavigationView {
             GeometryReader { geometry in
-                ScrollView {
+                ScrollView (showsIndicators: false){
                     VStack (spacing: 20) {
-                        MultiLineChartView(
-                            data: groupData,
-                            title: "Groups",
-                            legend: "Todos by group",
-                            style: style,
-                            form: CGSize(width: geometry.size.width - 40, height: 200),
-                            rateValue: 0,
-                            dropShadow: false
-                        )
-                        .foregroundColor(.red.opacity(0))
-                        
-                        LazyVGrid (columns: groupColumns, alignment: .leading) {
-                            ForEach(groups) { group in
-                                HStack {
-                                    Color(hex: group.color!)
-                                        .frame(width: 20, height: 20)
-                                        .cornerRadius(100)
-                                    Text("\(group.title!) (\(group.todos!.count))")
+                        VStack (spacing: 20) {
+                            MultiLineChartView(
+                                data: groupData,
+                                title: "Groups",
+                                legend: "Todos by group",
+                                style: style,
+                                form: CGSize(width: geometry.size.width - 40, height: 200),
+                                rateValue: 0,
+                                dropShadow: false
+                            )
+                            .foregroundColor(.red.opacity(0))
+                            
+                            LazyVGrid (columns: groupColumns, alignment: .leading) {
+                                ForEach(groups) { group in
+                                    HStack {
+                                        Color(hex: group.color!)
+                                            .frame(width: 20, height: 20)
+                                            .cornerRadius(100)
+                                        Text("\(group.title!) (\(group.todos!.count))")
+                                    }
                                 }
                             }
                         }
+                        
+                        BarChartView(
+                            data: ChartData(values: todosByGroup),
+                            title: "Total \(totalTodos) todos",
+                            style: style,
+                            form: CGSize(width: geometry.size.width - 40, height: 300),
+                            dropShadow: false
+                        )
                     }
                 }
                 .padding(20)
                 .navigationTitle("Statistics")
-                .preferredColorScheme(.dark)
             }
         }
     }
